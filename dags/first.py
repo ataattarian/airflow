96b6156d1bc219c129fc3ec01e1d41976d7ccf4a
ataattarian <ata.attarian78@gmail.com>
2023-11-21 15:19:51 +0330

    add swarm dag

--- dags/first.py
+++ dags/first.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.docker_operator import DockerOperator
+from airflow.contrib.operators.docker_swarm_operator import DockerSwarmOperator
 import pendulum
 
 
@@ -31,3 +32,18 @@ with DAG(
         network_mode="bridge"
         )
     
+
+with DAG(
+    dag_id="swarm",
+    schedule=None,
+    start_date=pendulum.datetime(2023, 11, 19, tz="UTC"),
+    catchup=False,
+    tags=["hi barbi"],
+) as dag:
+    t1 = DockerSwarmOperator(
+        api_version='auto',                
+        command='/bin/sleep 45',           
+        image='192.168.12.50:5000/web',             
+        auto_remove=True,                  
+        task_id='sleep_with_swarm',        
+    )
