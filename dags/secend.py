82064a1e8940972dfb88a5a1704b2b8cd008e1a8
ataattarian <ata.attarian78@gmail.com>
2023-11-25 13:36:12 +0330

    add secend dag

--- dags/secend.py
+++ dags/secend.py
@@ -9,7 +9,6 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
 }
 
 # Instantiate a DAG with the default_args
